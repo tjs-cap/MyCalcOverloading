6c5ba9761c6f162ea01b98a192a33781be54175a
TJS Capita <trevor.smith@capita.com>
2022-11-28 15:25:17 +0000

    Initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,22 @@
+public class Calculator {
+
+    public static int adds(int num1, int num2){
+        return num1 + num2;
+    }
+
+    public static int adds(int num1, int num2, int num3){
+        return num1 + num2 + num3;
+    }
+
+    public static float adds(float num1, float num2, float num3){
+        return num1 + num2 + num3;
+    }
+
+    public static float adds(float num1, float num2){
+        return num1 + num2 ;
+    }
+
+    public static float adds(int num1, float num2){
+        return num1 + num2;
+    }
+}
---
+++ src/Program.java
@@ -0,0 +1,9 @@
+public class Program {
+    public static void main(String[] args) {
+        System.out.println(Calculator.adds(3,4));
+        System.out.println(Calculator.adds(1,2,3));
+        System.out.println(Calculator.adds(1.2f,1.3f,1.4f));
+        System.out.println(Calculator.adds(1.2f,1.3f));
+        System.out.println(Calculator.adds(1,1.5f));
+    }
+}
